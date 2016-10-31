27598d594e6624d93dbb17eddb254f29dd56393f
Tetsuya Yamamoto <yamamoto@itmol.com>
2016-11-01 01:50:43 +0900

    commitby20161101

--- postgresql-entrypoint.sh
+++ postgresql-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+chown $POSTGRES_USER:$POSTGRES_USER $PGDATA
+su $POSTGRES_USER
+$POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -U $POSTGRES_USER --no-locale
 postgres
 
 #set -e
 # $POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -W -U $POSTGRES_USER --no-locale
 # $POSTGRES_HOME/bin/postgres
 #docker rm postgres | docker build -t molcalc/postgresql:0.1 -f postgresql-dockerfile . | docker run -d --name postgres -p 5433:5432 -v C:/Users/Jingring/git/molcalc/volumes/pgdata:/usr/local/pgsql/data molcalc/postgresql:0.1 | docker ps -a
+# $POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -W -U $POSTGRES_USER --no-locale
