e94d39ce8a246606e627ed66384e5906c80c1f47
Tetsuya Yamamoto <yamamoto@itmol.com>
2016-11-03 20:52:43 +0900

    PostgreSQL 0.1 finished.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # molcalc
 
-# testchange 20161031
+
+
+
+
+## コンテナ構築テスト
+
+### 1
+`docker run --name testvol -v /C:/mnt/dshare:/mnt busybox:buildroot-2014.02`
+
+### 2
+`docker build -t tetsuyajs/postgresql:0.1 -f postgresql-dockerfile .`
+
+### 3
+`docker run -it --name pgsql1 -p 5434:5432 --volumes-from testvol tetsuyajs/postgresql:0.1 bash`
+
+### x
+`docker ps -a`
+`docker exec -it pgsql1 bash`
+`docker rm pgsql1`
+
--- postgresql-entrypoint.sh
+++ postgresql-entrypoint.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
-chown $POSTGRES_USER:$POSTGRES_USER $PGDATA
-su $POSTGRES_USER
-$POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -U $POSTGRES_USER --no-locale
+# chown $POSTGRES_USER:$POSTGRES_USER $PGDATA
+
+#$POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -U $POSTGRES_USER --no-locale
+$POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -U $POSTGRES_USER
+
+echo "host all all 0.0.0.0/0 trust" >> $PGDATA/pg_hba.conf
+
+\pg_ctl -D "$PGDATA" \
+			-o "-c listen_addresses='localhost'" \
+			-w start
+
+: ${POSTGRES_USER:=postgres}
+: ${POSTGRES_DB:=$POSTGRES_USER}
+export POSTGRES_USER POSTGRES_DB
+
+psql=( psql -v ON_ERROR_STOP=1 )
+
+if [ "$POSTGRES_DB" != 'postgres' ]; then
+	"${psql[@]}" --username postgres <<-EOSQL
+		CREATE DATABASE "$POSTGRES_DB" ;
+	EOSQL
+	echo
+fi
+
+if [ "$POSTGRES_USER" = 'postgres' ]; then
+	op='ALTER'
+else
+	op='CREATE'
+fi
+
+"${psql[@]}" --username postgres <<-EOSQL
+	$op USER "$POSTGRES_USER" WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD' ;
+EOSQL
+echo
+
+pg_ctl -D "$PGDATA" -m fast -w stop
+
+cp -f /tmp/postgresql.conf $PGDATA
+cp -f /tmp/pg_hba.conf $PGDATA
+
 postgres
 
+#su $POSTGRES_USER
 #set -e
 # $POSTGRES_HOME/bin/initdb -D $PGDATA -E UTF8 -W -U $POSTGRES_USER --no-locale
 # $POSTGRES_HOME/bin/postgres
